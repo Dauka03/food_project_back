cc3270c3de2ea6820c4f2963e7640a9d3521117b
Dauka0 <dauka0300@gmail.comm>
2022-03-15 08:39:27 +0600

    Creatted modeles, admin panel

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mptt',
+
+    'menu',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [STATIC_DIR]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from . import models
+from mptt.admin import MPTTModelAdmin
 # Register your models here.
+
+
+class FoodPostAdmin(admin.ModelAdmin):
+    list_display = ['title','category','cost','quantity']
+admin.site.register(models.Category, MPTTModelAdmin)
+admin.site.register(models.Tag)
+admin.site.register(models.foodPost,FoodPostAdmin)
